99477a2150bafee623b953c55ab689e966fe2689
levaser <vovus402@gmail.com>
2024-09-08 20:33:53 +0300

    Added: new ui design

--- Assets/Scripts/CampaignMap/CampaignMapInstaller.cs
+++ Assets/Scripts/CampaignMap/CampaignMapInstaller.cs
@@ -1,4 +1,6 @@
+using Game.FinalMenues;
 using UnityEngine;
+using UnityEngine.UI;
 using VContainer;
 using VContainer.Unity;
 
@@ -7,10 +9,16 @@ namespace Game.CampaignMap
     public sealed class CampaignMapInstaller : LifetimeScope
     {
         [SerializeField]
+        private Button _backButton;
+
+        [SerializeField]
         private ButtonToConfigPair[] _buttonToConfigPairs;
 
         protected override void Configure(IContainerBuilder builder)
         {
+            builder.RegisterEntryPoint<BackButton>(Lifetime.Scoped)
+                .WithParameter(_backButton);
+
             foreach (var e in _buttonToConfigPairs)
             {
                 builder.RegisterEntryPoint<LevelStartButton>(Lifetime.Scoped)
